523ca99b6d5e59fd6c7d1a3c6d2720fa164a99f1
Yuhang Zhao <68658479+Kris-Zhao@users.noreply.github.com>
2026-09-23 15:10:17 -0400

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        char_freq = {}
+        left = 0
+        res = 0
+
+        for right in range(len(s)):
+            if s[right] not in char_freq:
+                char_freq[s[right]] = 0
+            char_freq[s[right]] += 1
+
+            # The window is valid as long as: window size – count of the most frequent character ≤ k
+            while right - left + 1 - max(char_freq.values()) > k:
+                char_freq[s[left]] -= 1
+                left += 1
+
+            res = max(res, right - left + 1)
+        
+        return res
